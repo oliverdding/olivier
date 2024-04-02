f9a46c535f9a58916405e860ce59ce1045036185
oliver ding <oliverdding@outlook.com>
2024-04-02 11:02:37 +0800

    fix: adjust initialization order

--- src/server/server.rs
+++ src/server/server.rs
@@ -2,14 +2,13 @@ use anyhow::{Context, Result};
 use migration::{Migrator, MigratorTrait};
 use tokio_graceful_shutdown::SubsystemHandle;
 use tracing::{error, info, warn};
-use utoipa::openapi::info;
 
 use crate::{config::AppConfig, router};
 
 use super::AppState;
 
 pub struct AppServer {
-    pub state: AppState,
+    router: axum::routing::Router,
     listener: tokio::net::TcpListener,
 }
 
@@ -17,18 +16,18 @@ impl AppServer {
     pub async fn new(config: AppConfig) -> Result<Self> {
         let listener = tokio::net::TcpListener::bind(config.service.get_socket_addr()?).await?;
 
-        let addr = listener.local_addr()?;
-        info!("listening on {}", addr);
-
         let state = AppState::new(config).await?;
-        Ok(Self { state, listener })
+
+        info!("migration started");
+        Migrator::up(&*state.database, None).await?;
+        info!("migration done");
+
+        let router = router::init(state);
+
+        Ok(Self { router, listener })
     }
 
     pub async fn run(self, subsys: SubsystemHandle) -> Result<()> {
-        info!("migration started");
-        Migrator::up(&*self.state.database, None).await?;
-        info!("migration done");
-
         info!("service started");
         tokio::select! {
             _ = subsys.on_shutdown_requested() => {
@@ -48,10 +47,12 @@ impl AppServer {
     }
 
     async fn serve(self) -> Result<()> {
-        let router = router::init(self.state);
-        axum::serve(self.listener, router)
+        let addr = self.listener.local_addr()?;
+        info!("listening on {}", addr);
+
+        axum::serve(self.listener, self.router)
             .await
-            .context("cannot start axum service at given address:port")?;
+            .context(format!("cannot start axum service at {}", addr))?;
         Ok(())
     }
 }
--- src/server/state.rs
+++ src/server/state.rs
@@ -1,6 +1,8 @@
 use crate::config::AppConfig;
 use anyhow::{Ok, Result};
 use sea_orm::Database;
+use tracing::info;
+
 use std::sync::Arc;
 
 #[derive(Clone)]
@@ -11,6 +13,7 @@ pub struct AppState {
 
 impl AppState {
     pub async fn new(config: AppConfig) -> Result<Self> {
+        info!("connecting to database");
         let database = Arc::new(Database::connect(&config.database.uri).await?);
 
         Ok(Self {
